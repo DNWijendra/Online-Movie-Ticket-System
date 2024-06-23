e3568fd80afa727697edbd28cf349e679ed20383
Dinithi Wijendra <wijendradn.21@uom.lk>
2024-06-23 16:57:32 +0530

    changes for the website

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -2,6 +2,7 @@ from.models import *
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
+from django import forms
 
 class New_User_Form(UserCreationForm):
     class Meta:
@@ -12,3 +13,6 @@ class New_Movie_Form(ModelForm):
     class Meta:
         model = Movie
         fields = '__all__'
+        widgets = {
+            'release_date': forms.DateInput(attrs={'type': 'date'})
+        }
